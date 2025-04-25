9395589d32c495457aaf2d7ccf846acccecdda48
Kemuel Batista <kemuellima20@gmail.com>
2025-04-25 15:40:17 -0300

    feat: s3 and cloudfront modularized

--- infra-as-code/outputs.tf
+++ infra-as-code/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_domain_name" {
   value       = data.aws_s3_bucket.bucket.bucket_domain_name
   sensitive   = false
-  description = "Nome do domínio do bucket"
+  description = "Nome do domínio do bucket S3"
 }
 
 output "bucket_region" {
--- infra-as-code/variables.tf
+++ infra-as-code/variables.tf
@@ -1,5 +1,5 @@
 variable "org_name" {
-  type = string
+  type    = string
   default = "ignite"
 }
 
